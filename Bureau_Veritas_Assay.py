99a70ca747f0f71bacb0ee25b0ca5b639a7050b9
Rithik Agarwal <ragarwal@bqewater.com>
2024-07-22 16:22:39 -0700

    transform
    Adding a function to transform different test types

--- Bureau_Veritas_Assay.py
+++ Bureau_Veritas_Assay.py
@@ -52,39 +52,21 @@ for file in file_list:
         continue
 
     try:
-        Sample_no = list(df['Sample #'].unique())
-        Sample_no = df[['Sample #', 'Client Sample #']].drop_duplicates().values.tolist()
-
-        project_no = df['Project #'].loc[0]
-        job_no = df['Job #'].loc[0]
-
         df['Result'] = df['Result'].apply(Functions.clean_result())
 
-        metal_df = df[
-            df['Test'].str.contains('Total', case=False, na=False)
-        ]
-        metal_df['Parameter'] = metal_df['Parameter'].str.replace('Total Extractable ', '')
+        df_pivot_metal, no_metal  = Functions.transform_parameters(df, test_type='Total', parameter ='Total Extractable ' )
+        
+        if df_pivot_metal.empty:
+            print("No total metal")
+            no_metal = True
+        
+        else:
+            list_of_columns = df_pivot_metal.columns.tolist()
 
-        metal_final_df = metal_df[['Sampling Date','Client Sample #', 'Test', 'Parameter', 'Result']]
+            list_of_columns = list_of_columns[5:]
 
-        df_pivot_metal = metal_final_df.pivot_table(index=['Sampling Date', 'Client Sample #'], columns='Parameter', values='Result', aggfunc=lambda x: ' '.join(str(v) for v in x)).reset_index()
-
-        df_sample_no = pd.DataFrame(Sample_no, columns=['Sample #', 'Client Sample #'])
-
-        df_pivot_metal = df_pivot_metal.merge(df_sample_no, left_on='Client Sample #', right_on='Client Sample #', how='left')
-        df_pivot_metal['Project #'] = project_no
-        df_pivot_metal['Job #'] = job_no
-
-        df_pivot_metal.insert(1, 'Sample #', df_pivot_metal.pop('Sample #'))
-        df_pivot_metal.insert(2, 'Project #', df_pivot_metal.pop('Project #'))
-        df_pivot_metal.insert(3, 'Job #', df_pivot_metal.pop('Job #'))
-
-        list_of_columns = df_pivot_metal.columns.tolist()
-
-        list_of_columns = list_of_columns[5:]
-
-        for i in list_of_columns:
-            df_pivot_metal[i] = df_pivot_metal[i].apply(lambda x: float(x) if not x.startswith('<') else x)
+            for i in list_of_columns:
+                df_pivot_metal[i] = df_pivot_metal[i].apply(lambda x: float(x) if not x.startswith('<') else x)
     except KeyError as e:
         print(f"A KeyError occurred: {str(e)}. Please make sure the necessary columns exist in the input data.")
         continue
@@ -109,31 +91,13 @@ for file in file_list:
 
 
     try:
-        dissolved_metal_df = df[
-            df['Test'].str.contains('Dissolved', case=False, na=False)
-        ]
-        no_dissolved_metal = False
-        if dissolved_metal_df.empty:
-            print("No dissolved metal metal")
+        df_pivot_metal_dissolved,no_dissolved_metal  = Functions.transform_parameters(df, test_type='Dissolved', parameter= 'Dissolved ')
+
+
+        if df_pivot_metal_dissolved.empty:
+            print("No dissolved metal")
             no_dissolved_metal = True
         else:
-            dissolved_metal_df['Parameter'] = dissolved_metal_df['Parameter'].str.replace('Dissolved ', '')
-
-            dissolved_metal_final_df = dissolved_metal_df[['Sampling Date','Client Sample #', 'Test', 'Parameter', 'Result']]
-
-            df_pivot_metal_dissolved = dissolved_metal_final_df.pivot_table(index=['Sampling Date', 'Client Sample #'], columns='Parameter', values='Result', aggfunc=lambda x: ' '.join(str(v) for v in x)).reset_index()
-            
-            df_sample_no = pd.DataFrame(Sample_no, columns=['Sample #', 'Client Sample #'])
-
-            df_pivot_metal_dissolved = df_pivot_metal_dissolved.merge(df_sample_no, left_on='Client Sample #', right_on='Client Sample #', how='left')
-            
-            #df_pivot_metal_dissolved['Sample #'] = Sample_no
-            df_pivot_metal_dissolved['Project #'] = project_no
-            df_pivot_metal_dissolved['Job #'] = job_no
-
-            df_pivot_metal_dissolved.insert(1, 'Sample #', df_pivot_metal_dissolved.pop('Sample #'))
-            df_pivot_metal_dissolved.insert(2, 'Project #', df_pivot_metal_dissolved.pop('Project #'))
-            df_pivot_metal_dissolved.insert(3, 'Job #', df_pivot_metal_dissolved.pop('Job #'))
 
             list_of_columns = df_pivot_metal_dissolved.columns.tolist()
 
@@ -164,33 +128,20 @@ for file in file_list:
         Functions.write_data(Writing_file, df_appended_metal_dissolved, sheet='Dissolved')
 
     try:
-        Conventional_df = df[
-            ~(df['Test'].str.contains('Dissolved|Total|Mercury', case=False, na=False))
-        ]
-        Conventional_final_df = Conventional_df[['Sampling Date','Client Sample #', 'Test', 'Parameter', 'Result']]
-
-        df_pivot_conventional = Conventional_final_df.pivot_table(index=['Sampling Date', 'Client Sample #'], columns='Parameter', values='Result', aggfunc=lambda x: ' '.join(str(v) for v in x)).reset_index()
         
-        df_sample_no = pd.DataFrame(Sample_no, columns=['Sample #', 'Client Sample #'])
+        df_pivot_conventional,no_conventional  = Functions.transform_parameters(df, test_type='Dissolved|Total|Mercury', parameter= 'Dissolved ')
 
-        df_pivot_conventional = df_pivot_conventional.merge(df_sample_no, left_on='Client Sample #', right_on='Client Sample #', how='left')
-        
-        #df_pivot_conventional['Sample #'] = Sample_no
-        df_pivot_conventional['Project #'] = project_no
-        df_pivot_conventional['Job #'] = job_no
+        if df_pivot_conventional.empty:
+            print("No dissolved metal")
+            no_conventional = True
+        else:
+            list_of_columns = df_pivot_conventional.columns.tolist()
 
+            list_of_columns = list_of_columns[5:]
 
-        df_pivot_conventional.insert(1, 'Sample #', df_pivot_conventional.pop('Sample #'))
-        df_pivot_conventional.insert(2, 'Project #', df_pivot_conventional.pop('Project #'))
-        df_pivot_conventional.insert(3, 'Job #', df_pivot_conventional.pop('Job #'))
+            for i in list_of_columns:
+                df_pivot_conventional[i] = df_pivot_conventional[i].apply(lambda x: float(x) if not x.startswith('<') else x)
 
-        list_of_columns = df_pivot_conventional.columns.tolist()
-
-        list_of_columns = list_of_columns[5:]
-
-        for i in list_of_columns:
-            df_pivot_conventional[i] = df_pivot_conventional[i].apply(lambda x: float(x) if not x.startswith('<') else x)
-        
     except KeyError as e:
         print(f"A KeyError occurred: {str(e)}. Please make sure the necessary columns exist in the input data.")
         input("Press Enter to end ")
--- util/Functions.py
+++ util/Functions.py
@@ -30,3 +30,44 @@ def write_data(Writing_file, df, sheet):
             print(f"An error occurred: {e}")
             input('Press any key to enc')
             exit(1)
+
+def transform_parameters(df, test_type, parameter):
+    
+    Sample_no = list(df['Sample #'].unique())
+    Sample_no = df[['Sample #', 'Client Sample #']].drop_duplicates().values.tolist()
+
+    project_no = df['Project #'].loc[0]
+    job_no = df['Job #'].loc[0] 
+    
+    df = df[
+        df['Test'].str.contains(test_type, case=False, na=False)
+    ]
+
+    parameter_check = False
+
+    if df.empty:
+        parameter_check = True
+        df_pivot = pd.DataFrame()
+
+        return df_pivot, parameter_check
+    else:
+
+        if test_type != 'Dissolved|Total|Mercury':
+            df['Parameter'] = df['Parameter'].str.replace(parameter, '')
+
+        final_df = df[['Sampling Date','Client Sample #', 'Test', 'Parameter', 'Result']]
+
+        df_pivot = final_df.pivot_table(index=['Sampling Date', 'Client Sample #'], columns='Parameter', values='Result', aggfunc=lambda x: ' '.join(str(v) for v in x)).reset_index()
+        
+        df_sample_no = pd.DataFrame(Sample_no, columns=['Sample #', 'Client Sample #'])
+
+        df_pivot = df_pivot.merge(df_sample_no, left_on='Client Sample #', right_on='Client Sample #', how='left')
+        df_pivot['Project #'] = project_no
+        df_pivot['Job #'] = job_no
+
+        df_pivot.insert(1, 'Sample #', df_pivot.pop('Sample #'))
+        df_pivot.insert(2, 'Project #', df_pivot.pop('Project #'))
+        df_pivot.insert(3, 'Job #', df_pivot.pop('Job #'))
+
+    return df_pivot, parameter_check
+        
